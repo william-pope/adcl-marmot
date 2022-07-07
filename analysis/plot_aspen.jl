f351c129d14f129f57bde00f464bede76e9e6118
ADCL <william.pope@colorado.edu>
2022-07-07 07:25:32 -0600

    marmot 7 jul

--- analysis/plot_aspen.jl
+++ analysis/plot_aspen.jl
@@ -4,28 +4,50 @@ using Plots
 using BSON: @save, @load
 using CSV
 using DataFrames
-include("../HJB-planner/HJB_functions.jl")
+
+include("../HJB-planner/HJB_generator_functions.jl")
 
 hjb_path_mac = "/Users/willpope/Desktop/Research/marmot-algs/HJB-planner/bson/"
-@load hjb_path_mac*"env.bson" env
-@load hjb_path_mac*"veh.bson" veh
+hjb_path_nuc = "/home/adcl/Documents/marmot-algs/HJB-planner/bson/"
+hjb_path = hjb_path_nuc
 
-hist_path_mac = "/Users/willpope/Desktop/Research/marmot-ros/controller_pkg/histories/stored/"
-@load hist_path_mac*"s_hist.bson" s_hist
+@load hjb_path*"env.bson" env
+@load hjb_path*"veh.bson" veh
 
-vrpn_csv = CSV.File(hist_path_mac*"s_hist_vrpn.csv")
-s_hist_vrpn = []
-for s_kv in vrpn_csv
-    push!(s_hist_vrpn, s_kv)
+hist_path_mac = "/Users/willpope/Desktop/Research/marmot-ros/controller_pkg/histories/"
+hist_path_nuc = "/home/adcl/catkin_ws/src/marmot-ros/controller_pkg/histories/"
+hist_path = hist_path_nuc
+
+# load planner histories
+@load hist_path*"s_hist.bson" s_hist
+veh_hist = []
+ped1_hist = []
+for s_k in s_hist
+    push!(veh_hist, s_k[1:3])
+    push!(ped1_hist, s_k[4:5])
 end
 
-@show size(s_hist,1)
-@show size(s_hist_vrpn,1)
+# load VRPN histories
+csv = CSV.File(hist_path*"veh_hist_vrpn.csv")
+veh_hist_vrpn = []
+for s_k in csv
+    push!(veh_hist_vrpn, s_k)
+end
+
+csv = CSV.File(hist_path*"ped1_hist_vrpn.csv")
+ped1_hist_vrpn = []
+for s_kv in csv
+    push!(ped1_hist_vrpn, s_kv)
+end
+
+@show size(veh_hist,1)
+@show size(veh_hist_vrpn,1)
 
 # plot optimal path from y_0 to target set
 p_path = plot(aspect_ratio=:equal, size=(750,1050), 
             xlabel="x-axis [m]", ylabel="y-axis [m]",
             title="Vicon Output", 
+            legend=false,
             titlefontsize = 20,
             legend_font_pointsize = 11,
             top_margin = -5*Plots.mm,
@@ -37,16 +59,24 @@ plot_polygon(p_path, env.T_xy, 2, :green, "Target Set")
 # plot_polygon(p_path, env.O_vec[2], 2, :red, "")
 # plot_polygon(p_path, env.O_vec[3], 2, :red, "")]
 
-plot!(p_path, getindex.(s_hist,1), getindex.(s_hist,2),
+plot!(p_path, getindex.(veh_hist,1), getindex.(veh_hist,2).+7,
     linewidth=0, markershape=:circle, markersize=3, markerstrokewidth=0 ,
     label="Planning Points")
 
-plot!(p_path, getindex.(s_hist_vrpn,1), getindex.(s_hist_vrpn,2),
+plot!(p_path, getindex.(veh_hist_vrpn,1), getindex.(veh_hist_vrpn,2).+7,
+    linewidth=1,
+    label="Vicon Output")
+
+plot!(p_path, getindex.(ped1_hist,1), getindex.(ped1_hist,2).+7,
+    linewidth=0, markershape=:circle, markersize=3, markerstrokewidth=0 ,
+    label="Planning Points")
+
+plot!(p_path, getindex.(ped1_hist_vrpn,1), getindex.(ped1_hist_vrpn,2).+7,
     linewidth=1,
     label="Vicon Output")
 
 # plot initial state
-V_ci = pose_to_corners(s_hist[1], veh)
+V_ci = pose_to_edges(s_hist[1], veh)
 V_i = [[V_ci[1][1] V_ci[1][2]];
     [V_ci[2][1] V_ci[2][2]];
     [V_ci[3][1] V_ci[3][2]];
@@ -58,7 +88,7 @@ plot!(p_path, [s_hist[1][1]], [s_hist[1][2]],
     label="Initial State")
 
 # plot final state
-V_cf = pose_to_corners(s_hist[end], veh)
+V_cf = pose_to_edges(s_hist[end], veh)
 V_f = [[V_cf[1][1] V_cf[1][2]];
     [V_cf[2][1] V_cf[2][2]];
     [V_cf[3][1] V_cf[3][2]];
