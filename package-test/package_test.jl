5d7e014c963a1c1ea9edfabc5d5d5192880c5f7f
Will Pope <william.pope@colorado.edu>
2022-10-26 14:36:31 -0600

    benchmarking package

--- package-test/package_test.jl
+++ package-test/package_test.jl
@@ -61,31 +61,32 @@ function get_actions(x, Dt, veh)
     phi_lim_n = atan(Dtheta_lim * 1/Dt * 1/abs(vn) * veh.l)
     phi_lim_n = clamp(phi_lim_n, 0.0, phi_max)
 
-    ro_action_set = [[-phi_lim, 0.0],       # Dv = 0.0
-                    [-2/3*phi_lim, 0.0],
-                    [-1/3*phi_lim, 0.0],
-                    [0.0, 0.0],
-                    [1/3*phi_lim, 0.0],
-                    [2/3*phi_lim, 0.0],
-                    [phi_lim, 0.0],
+    actions = SVector{21, SVector{2, Float64}}(
+        (-phi_lim, 0.0),       # Dv = 0.0
+        (-2/3*phi_lim, 0.0),
+        (-1/3*phi_lim, 0.0),
+        (0.0, 0.0),
+        (1/3*phi_lim, 0.0),
+        (2/3*phi_lim, 0.0),
+        (phi_lim, 0.0),
 
-                    [-phi_lim_n, -Dv_lim],        # Dv = +Dv
-                    [-2/3*phi_lim_n, -Dv_lim],
-                    [-1/3*phi_lim_n, -Dv_lim],
-                    [0.0, -Dv_lim],
-                    [1/3*phi_lim_n, -Dv_lim],
-                    [2/3*phi_lim_n, -Dv_lim],
-                    [phi_lim_n, -Dv_lim],
+        (-phi_lim_n, -Dv_lim),        # Dv = +Dv
+        (-2/3*phi_lim_n, -Dv_lim),
+        (-1/3*phi_lim_n, -Dv_lim),
+        (0.0, -Dv_lim),
+        (1/3*phi_lim_n, -Dv_lim),
+        (2/3*phi_lim_n, -Dv_lim),
+        (phi_lim_n, -Dv_lim),
 
-                    [-phi_lim_p, Dv_lim],        # Dv = -Dv
-                    [-2/3*phi_lim_p, Dv_lim],
-                    [-1/3*phi_lim_p, Dv_lim],
-                    [0.0, Dv_lim],
-                    [1/3*phi_lim_p, Dv_lim],
-                    [2/3*phi_lim_p, Dv_lim],
-                    [phi_lim_p, Dv_lim]]
+        (-phi_lim_p, Dv_lim),        # Dv = -Dv
+        (-2/3*phi_lim_p, Dv_lim),
+        (-1/3*phi_lim_p, Dv_lim),
+        (0.0, Dv_lim),
+        (1/3*phi_lim_p, Dv_lim),
+        (2/3*phi_lim_p, Dv_lim),
+        (phi_lim_p, Dv_lim))
 
-    return ro_action_set
+    return actions
 end
 
 # define cost function
@@ -98,15 +99,15 @@ end
 # MAIN ---
 path = "/Users/willpope/Desktop/Research/marmot-algs/package-test"
 
-solve_flag = 0
-plot_value = 0
+solve_flag = 1
+plot_value = 1
 
-plan_flag = 0
-plot_path = 0
+plan_flag = 1
+plot_path = 1
 
 # solver
 if solve_flag == 1
-    value_array, a_ind_opt_array = solve_HJB_PDE(get_actions, get_cost, Dt, env, veh, sg, Dval_tol, max_solve_steps)
+    @time value_array, a_ind_opt_array = solve_HJB_PDE(get_actions, get_cost, Dt, env, veh, sg, Dval_tol, max_solve_steps)
     @save path * "/bson/HJB_solution.bson" value_array a_ind_opt_array
 else
     @load path * "/bson/HJB_solution.bson" value_array a_ind_opt_array
@@ -131,25 +132,51 @@ end
 # benchmarking
 #=
 baseline performance:
-  - discrete_time_EoM:  30.296 ns   (0 allocations)
-  - propagate_state:    494.078 ns  (8 allocations)
-  - optimize_action:    19.243 us   (224 allocations)
-  - get_actions:        1.044 us    (22 allocations)
-  - HJB_policy:         20.908 us   (256 allocation)
-  - rollout_policy:     8.268 us    (110 allocations)
+- discrete_time_EoM:    30.296 ns   (0 allocations)
+- propagate_state:      494.078 ns  (8 allocations)
+- interp_value:         349.815 ns  (5 allocations)
+- optimize_action:      19.243 us   (224 allocations) [21 actions]
+- get_actions:          1.044 us    (22 allocations)
+- HJB_policy:           20.908 us   (256 allocation)
+- rollout_policy:       8.268 us    (110 allocations)
+
+improved performance:
+- discrete_time_EoM:    28.974 ns   (0 allocations)
+- propagate_state:      163.798 ns  (1 allocation) [think in MVector]
+- interp_value:         349.815 ns  (5 allocations) [all from GridInterpolations.jl]
+- optimize_action:      11.844 us   (98 allocations) [21 actions] [basically as good as possible]
+- get_actions:          49.795 ns   (0 allocations)
+- HJB_policy:           12.340 us   (110 allocations) [21 actions]
+- rollout_policy:       4.285 us    (42 allocations)
+
+structure:
+- HJB_policy:                           12.340 us   (110 allocations)
+    - get_actions (x1):                      49.795 ns   (0 allocations)
+    - optimize_action (x1):                  11.844 us   (98 allocations)
+        - propagate_state (x21):        163.798 ns  (1 allocation)
+            - discrete_time_EoM (x4):   28.974 ns   (0 allocations)
+        - interp_value (x21):           349.815 ns  (5 allocations)
 =#
 
-x_k = SVector(2.3, 1.0, deg2rad(15), 2.0)
-a_k = [0.327, 0.5]
-Dv_RC = 0.5
+# actions = get_actions(x_k, Dt, veh)
+# a_ind_array = collect(1:length(actions))
 
-actions = get_actions(x_k, Dt, veh)
-a_ind_array = collect(1:length(actions))
+# x_k = SVector{4, Float64}(2.3, 1.0, deg2rad(15), 2.0)
+# a_k = actions[16]
+# Dv_RC = 0.5
 
 # @btime discrete_time_EoM($x_k, $a_k, $Dt, $veh)
 
+# x_k1, x_k1_subpath = propagate_state(x_k, a_k, Dt, veh)
 # @btime propagate_state($x_k, $a_k, $Dt, $veh)
 
+# val_x = interpolate(sg.state_grid, value_array, x_k)
+# @btime interpolate($sg.state_grid, $value_array, $x_k)
+
+# val_x = interp_value(x_k, value_array, sg)
+# @btime interp_value($x_k, $value_array, $sg)
+
+# a_ind_opt, val_x = optimize_action(x_k, a_ind_array, actions, get_cost, Dt, value_array, veh, sg)
 # @btime optimize_action($x_k, $a_ind_array, $actions, $get_cost, $Dt, $value_array, $veh, $sg)
 
 # @btime get_actions($x_k, $Dt, $veh)
